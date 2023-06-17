4ca9fd80b6565ee67aa846ad112006b8f9f0418e
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-06-17 17:21:24 +0530

    Creating UI for app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-
+import MovieCard from './MovieCard';
+import Navbar from './Navbar';
+import {data} from '../data';
 
 function App() {
   return (
     <div className="App">
-      <h1>Project setup</h1>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map((movie) => 
+            <MovieCard movie={movie} key={data.indexOf(movie)} />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
