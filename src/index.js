a77229b5fbe436121cdd86c04b6b4ccf7f4d8643
Shivaraj M Shetty <shivaraj916462@gmail.com>
2023-06-17 23:17:43 +0530

    creating store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore as createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+//create the store
+const store = createStore(movies); //createStore takes reducer as argument
+console.log(store);
+console.log(store.getState()); //to fetch the state details
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
